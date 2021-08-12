39c4c955193292fbe63a2ec781a0e73a43ba4e4b
Thiago <65466044+thiagobezerra2@users.noreply.github.com>
2021-08-12 10:51:57 -0300

    Add files via upload

---
+++ Bankfuncional/Bankfuncional/Classe/Conta.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankfuncional
+{
+    public class Conta
+    {
+        public string titular;
+        public int agencia;
+        public int conta; 
+        public double saldo;
+
+        //Função Sacar 
+        public bool Sacar(double valorSaque)
+        {
+            if (this.saldo >= valorSaque)
+            {
+                this.saldo -= valorSaque;
+                Console.WriteLine("Saque realizado com sucesso.");
+                Console.WriteLine("O saldo atualda da conta é de {0}", this.saldo);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Função não permitida, saldo insuficiente");
+                return false;
+            }
+
+        }
+
+        //FUNÇÃO DEPOSITAR (SEM RETORNO)
+        public void Depositar(double valorDeposito)
+        {
+            Console.WriteLine("Depósito realizado com Sucesso.");
+            this.saldo -= valorDeposito;
+        }
+           
+
+    }
+
+
+}
---
+++ Bankfuncional/Bankfuncional/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Bankfuncional
+{
+    class Program
+    {
+        static List<Conta> listContas = new List<Conta>();
+        static void Main(string[] args)
+        {
+            string telaUsuario = opcaoUsuario();
+
+            while(telaUsuario.ToUpper() != "X")
+            {
+                switch(telaUsuario)
+                {
+                    case "1":
+                        InserirConta();
+                        break;
+                    case "2":
+                        Sacar();
+                        break;
+                    case "3":
+                        Depositar();
+                        break;
+                    case"L":
+                        Console.Clear();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                telaUsuario = opcaoUsuario();
+            }
+            static void InserirConta()
+            { 
+                Console.WriteLine("Inserir nova conta");
+
+                Console.Write("Digite o seu Nome: ");
+                string entradaNome = Console.ReadLine();
+                listContas.Add(novaConta);
+                
+            }
+            static void Sacar()
+            {
+                Console.Write("Digite o número da conta: ");
+                int indiceConta = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o valor a ser sacado: ");
+                double valorSaque = double.Parse(Console.ReadLine());
+
+           listContas[indiceConta].Sacar(valorSaque);
+            }
+            static void Depositar()
+            {
+                Console.Write("Digite o numero da conta");
+                int indiceConta = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o valor a ser Depositado");
+                double indiceConta = double.Parse(Console.ReadLine());
+
+           listContas[indiceConta].Depositar(valorDeposito);
+            }  
+            static string opcaoUsuario()
+            {
+                Console.WriteLine();
+                Console.WriteLine("Informe a opção desejada: ");
+
+                Console.WriteLine("1- Inserir nova conta");
+                Console.WriteLine("2- Sacar");
+                Console.WriteLine("3- Depositar");
+                Console.WriteLine("L-- Limpar a tela");
+                Console.WriteLine("X- Sair");
+                Console.WriteLine();
+
+                string telaUsuario = Console.ReadLine().ToUpper();
+                Console.WriteLine();
+                return opcaoUsuario;
+            }
+        }
+    }
+}
